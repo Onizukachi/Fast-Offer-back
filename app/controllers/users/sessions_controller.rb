fae1932aa665132c8c013a5de9d8f837e8ae3d1e
Alexei <dark_sao@mail.ru>
2024-06-30 17:15:14 +0300

    denylist strategy for jwt

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -14,15 +14,20 @@ module Users
     end
 
     def respond_to_on_destroy
-      if current_user
-        render json: {
-          message: 'logged out successfully'
-        }, status: :ok
-      else
-        render json: {
-          message: "Couldn't find an active session."
-        }, status: :unauthorized
-      end
+      header = request.headers['authorization'].split(' ')[1]
+      key = Rails.application.credentials.fetch(:secret_key_base)
+      jwt_payload = JWT.decode(header, key).first
+      current_user = User.find(jwt_payload['sub'])
+      log_out_success && return if current_user
+      log_out_failure
+    end
+
+    def log_out_success
+      render json: { message: 'You are logged out.' }, status: :ok
+    end
+
+    def log_out_failure
+      render json: { message: 'logout failed !!' }, status: :unauthorized
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_29_132431) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_30_124326) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jwt_denylist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
+  end
 
   create_table "positions", force: :cascade do |t|
     t.string "title"
